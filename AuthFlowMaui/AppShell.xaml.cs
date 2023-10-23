e0de529c25254981b63b187497caea5858e0ed45
horatiu <horatiu.cod@gmail.com>
2023-10-23 22:07:54 +0300

    add routing for pages, using routing and DI

--- AuthFlowMaui/AppShell.xaml.cs
+++ AuthFlowMaui/AppShell.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace AuthFlowMaui
+﻿using AuthFlowMaui.Pages;
+
+namespace AuthFlowMaui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            //using DI and shell navigation
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(LoadingPage), typeof(LoadingPage));
+            Routing.RegisterRoute(nameof(ListingPage), typeof(ListingPage));
+            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
         }
     }
 }
