386889da939e9b5c6e9b06db11da1495c88d1e96
horatiu <horatiu.cod@gmail.com>
2023-10-26 20:00:17 +0300

    implement IPlatformHttpMessageHandler on IosPlatformHttpMessageHandler
    GetHttpMessageHandler by newing NSUrlSessionHandler, invoke TrustOverrideForUrl

---
+++ AuthFlowMaui/Platforms/iOS/IosPlatformHttpMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿using AuthFlowMaui.Services;
+using Security;
+
+namespace AuthFlowMaui.Platforms.iOS;
+
+public class IosPlatformHttpMessageHandler : IPlatformHttpMessageHandler
+{
+    public HttpMessageHandler GetHttpMessageHandler() => new NSUrlSessionHandler
+    {
+        TrustOverrideForUrl = (NSUrlSessionHandler sender, string url, SecTrust trust) => url.StartsWith("https://localhost")
+    };
+}
