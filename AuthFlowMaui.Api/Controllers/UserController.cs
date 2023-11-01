267228ff36bed20a1d1af76bc899032c9d8d865b
horatiu <horatiu.cod@gmail.com>
2023-11-01 16:11:55 +0200

    add UserController

---
+++ AuthFlowMaui.Api/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthFlowMaui.Api.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
