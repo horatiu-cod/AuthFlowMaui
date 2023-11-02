037c4da57694e8c0719a1143d3a55d6c0d98a711
horatiu <horatiu.cod@gmail.com>
2023-11-02 14:39:40 +0200

    add KeycloakUserTokenRequestDtos

---
+++ AuthFlowMaui.Shared/TokenDtos/KeycloakUserTokenRequestDtos.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.TokenDtos;
+
+public class KeycloakUserTokenRequestDtos
+{
+    [JsonPropertyName("grant_type")]
+    public string GrantType { get; set;}
+    [JsonPropertyName("client_id")]
+    public string ClientId { get; set;}
+    [JsonPropertyName("client_secret")]
+    public string ClientSecret { get; set; }
+    [JsonPropertyName("username")]
+    public string Username { get; set; }
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
