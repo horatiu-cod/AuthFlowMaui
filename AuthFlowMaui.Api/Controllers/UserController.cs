5b88bae55fb2ebfcf7e2cecfe40a58991e02808f
horatiu <horatiu.cod@gmail.com>
2023-11-02 14:18:32 +0200

    delete weather forecat biolerplate

--- AuthFlowMaui.Api/Controllers/UserController.cs
+++ AuthFlowMaui.Api/Controllers/UserController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthFlowMaui.Shared.Abstractions;
+using AuthFlowMaui.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace AuthFlowMaui.Api.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IKeycloakTokenService _keycloakTokenService;
+
+        public UserController(IKeycloakTokenService keycloakTokenService)
         {
-            return View();
+            _keycloakTokenService = keycloakTokenService;
+        }
+
+        [HttpPost("token")]
+        public async Task<IActionResult> AuthorizeAsync([FromBody] KeycloakUserDtos keycloakUserDtos)
+        {
+            try
+            {
+                var response = await _keycloakTokenService
+                    .GetTokenResponseAsync(keycloakUserDtos)
+                    .ConfigureAwait(false);
+
+                return new OkObjectResult(response);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Authorization failed");
+            }
+        }
+
+        [Authorize]
+        [HttpGet("check/authorization")]
+        public IActionResult CheckAuthorization()
+        {
+            return new OkObjectResult(HttpStatusCode.OK);
         }
     }
 }
