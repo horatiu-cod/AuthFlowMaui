4670ab86ac34825cc8e69662e1b42223838afccf
horatiu <horatiu.cod@gmail.com>
2023-11-02 14:22:28 +0200

    KeycloakAccessTokenConst
     public static string GrantType => "grant_type";
     public static string GrantTypePassword => "password";
     public static string ClientId => "client_id";
     public static string ClientSecret => "client_secret";
     public static string Username => "username";
     public static string Password => "password";

---
+++ AuthFlowMaui.Shared/Constants/KeycloakAccessTokenConst.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthFlowMaui.Shared.Constants;
+
+public static class KeycloakAccessTokenConst
+{
+    public static string GrantType => "grant_type";
+    public static string GrantTypePassword => "password";
+    public static string ClientId => "client_id";
+    public static string ClientSecret => "client_secret";
+    public static string Username => "username";
+    public static string Password => "password";
+}
