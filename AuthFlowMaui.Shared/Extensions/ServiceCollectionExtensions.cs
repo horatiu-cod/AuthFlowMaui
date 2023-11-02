9a1f2480919fbfcf81385e38ad6f50197ada6a96
horatiu <horatiu.cod@gmail.com>
2023-11-02 14:24:40 +0200

    add ServiceCollectionExtensions
    register AddHttpClient and AddScoped<IKeycloakTokenService, KeycloakTokenService>();

---
+++ AuthFlowMaui.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AuthFlowMaui.Shared.Abstractions;
+using AuthFlowMaui.Shared.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthFlowMaui.Shared.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddServices (this IServiceCollection services)
+    {
+        services.AddHttpClient ();
+        services.AddScoped<IKeycloakTokenService, KeycloakTokenService>();
+
+        return services;
+    }
+}
