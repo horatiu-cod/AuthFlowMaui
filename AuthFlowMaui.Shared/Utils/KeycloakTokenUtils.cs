a36b207ed5be1012f1902435b6734efd8ab196ca
horatiu <horatiu.cod@gmail.com>
2023-11-04 11:29:20 +0200

    added KeyValuePairs for FormUrlContent body methods

---
+++ AuthFlowMaui.Shared/Utils/KeycloakTokenUtils.cs
@@ -0,0 +1,32 @@
+﻿using AuthFlowMaui.Shared.Constants;
+using AuthFlowMaui.Shared.TokenDtos;
+
+namespace AuthFlowMaui.Shared.Utils;
+
+public static class KeycloakTokenUtils
+{
+    public static FormUrlEncodedContent GetUserTokenRequestBody(KeycloakUserTokenRequestDtos tokenRequestDtos)
+    {
+        //to request token we need FormUrlEncodedContentType, we create a list of
+        var keyValuePairs = new List<KeyValuePair<string, string>>()
+        {
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.GrantType, tokenRequestDtos.GrantType),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientId, tokenRequestDtos.ClientId),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientSecret, tokenRequestDtos.ClientSecret),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.Username, tokenRequestDtos.Username),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.Password, tokenRequestDtos.Password)
+        };
+        return new FormUrlEncodedContent(keyValuePairs);
+    }
+    public static FormUrlEncodedContent GetClientTokenRequestBody(KeycloakClientTokenRequestDto tokenRequestDto)
+    {
+        var keyValuePairs = new List<KeyValuePair<string, string>>()
+        {
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.GrantType, tokenRequestDto.GrantType),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientId, tokenRequestDto.ClientId),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientSecret, tokenRequestDto.ClientSecret)
+
+        };
+        return new FormUrlEncodedContent(keyValuePairs);
+    }
+}
