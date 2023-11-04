2b5731d4d2462c610010e7884a93673bdcee8be0
horatiu <horatiu.cod@gmail.com>
2023-11-04 11:27:53 +0200

    removed unused namespaces

--- AuthFlowMaui.Shared/Extensions/ServiceCollectionExtensions.cs
+++ AuthFlowMaui.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -2,11 +2,6 @@
 using AuthFlowMaui.Shared.Services;
 using AuthFlowMaui.Shared.Settings;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AuthFlowMaui.Shared.Extensions;
 
