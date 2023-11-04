bbc24a3e54146e73debddfb77e3f8f631a389106
horatiu <horatiu.cod@gmail.com>
2023-11-04 09:52:00 +0200

    added JwtBearerConfig reflecting the JwtBearer section appsettings

---
+++ AuthFlowMaui.Api/Settings/JwtBearerConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthFlowMaui.Api.Settings;
+
+public class JwtBearerConfig
+{
+    public string Authority { get; set; }
+    public bool SaveToken { get; set; }
+    public bool RequireHttpsMetadata { get; set; }
+    public bool ValidateIssuer { get; set; }
+    public bool ValidateAudience { get; set; }
+    public string ValidAudience { get; set; }
+    public string ValidIssuer { get; set; }
+    public bool ValidateLifetime { get; set; }
+    public bool ValidateIssuerSigningKey { get; set; }
+}
