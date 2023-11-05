1c970b854a47164ab7b4a428e3d420a31719213c
horatiu <horatiu.cod@gmail.com>
2023-11-05 15:14:39 +0200

    cleaned

--- AuthFlowMaui.Shared/Abstractions/IKeycloakTokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using AuthFlowMaui.Shared.Dtos;
-using AuthFlowMaui.Shared.TokenDtos;
-
-namespace AuthFlowMaui.Shared.Abstractions;
-public interface IKeycloakTokenService
-{
-    Task<KeycloakTokenResponseDto?> GetTokenResponseAsync(KeycloakUserDtos keycloakUserDtos);
-}
