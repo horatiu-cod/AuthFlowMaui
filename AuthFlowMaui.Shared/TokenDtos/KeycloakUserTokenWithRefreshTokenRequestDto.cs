b507242ef03e30a8b5951ee7ddcaffe866c12585
horatiu <horatiu.cod@gmail.com>
2023-11-05 15:53:52 +0200

    added KeycloakUserTokenWithRefreshTokenRequestDto

---
+++ AuthFlowMaui.Shared/TokenDtos/KeycloakUserTokenWithRefreshTokenRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.TokenDtos;
+
+public class KeycloakUserTokenWithRefreshTokenRequestDto
+{
+    [JsonPropertyName("grant_type")]
+    public string? GrantType { get; set; }
+    [JsonPropertyName("client_id")]
+    public string? ClientId { get; set; }
+    [JsonPropertyName("client_secret")]
+    public string? ClientSecret { get; set; }
+    [JsonPropertyName("refresh_token")]
+    public string? RefreshToken { get; set; }
+
+}
