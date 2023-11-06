45c809e7930a6ce96f1eb16eeff63d0fa6cf4720
horatiu <horatiu.cod@gmail.com>
2023-11-06 17:20:32 +0200

    added TokenService
    check the if the token is expired/valid

---
+++ AuthFlowMaui/Shared/Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using AuthFlowMaui.Shared.Utils;
+using System.Security.Claims;
+
+namespace AuthFlowMaui.Shared.Services;
+
+public interface ITokenService
+{
+    ClaimsPrincipal ValidateToken(string token);
+    Task<MethodResult> ValidateTokenAsync(string token);
+}
---
+++ AuthFlowMaui/Shared/Services/TokenService.cs
@@ -0,0 +1,54 @@
+﻿using AuthFlowMaui.Shared.Utils;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AuthFlowMaui.Shared.Services;
+
+public class TokenService : ITokenService
+{
+    public ClaimsPrincipal ValidateToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var validationParameter = new TokenValidationParameters
+        {
+            ValidateLifetime = true,
+        };
+        try
+        {
+            return tokenHandler.ValidateToken(token, validationParameter, out _);
+        }
+        catch (Exception)
+        {
+
+            return null;
+        }
+    }
+    public async Task<MethodResult> ValidateTokenAsync(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var validationParameter = new TokenValidationParameters
+        {
+            ValidateLifetime = true,
+        };
+        try
+        {
+            var result = await tokenHandler.ValidateTokenAsync(token, validationParameter);
+            if (result.IsValid)
+            {
+                return MethodResult.Success();
+            }
+            else
+            {
+                return MethodResult.Fail("");
+            }
+        }
+        catch (Exception ex)
+        {
+
+            return MethodResult.Fail($"{ex.Message}");
+
+        }
+
+    }
+}
