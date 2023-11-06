37b188fac7b277bc355276cce6cccb5301904c12
horatiu <horatiu.cod@gmail.com>
2023-11-06 18:31:17 +0200

    added ConnectivityTest, basic checks
    todo -> check to refactor eventhandler to community.toolkit.mvvm messenger

---
+++ AuthFlowMaui/Shared/Utils/ConnectivityTest.cs
@@ -0,0 +1,62 @@
+﻿namespace AuthFlowMaui.Shared.Utils;
+
+
+// if Android update:
+// Platforms/Android/MainApplication.cs
+// [assembly: UserPermission(Android.Manifest.Permission.accessNetworkState]
+// or
+// Platforms/Android/AndroidManifest.xml
+// <users-permission android: name="android:permission.ACCESS_NETWORK_STATE />
+public class ConnectivityTest
+{
+    public ConnectivityTest() => Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    ~ConnectivityTest() => Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+
+    IEnumerable<ConnectionProfile> profiles = Connectivity.Current.ConnectionProfiles;
+    NetworkAccess accessType = Connectivity.Current.NetworkAccess;
+
+    private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+    {
+        if (e.NetworkAccess == NetworkAccess.ConstrainedInternet)
+            Console.WriteLine("Internet access is available but is limited");
+        else if (e.NetworkAccess == NetworkAccess.Local)
+            Console.WriteLine("Only local internet is available");
+        else if (e.NetworkAccess != NetworkAccess.Internet) 
+            Console.WriteLine("Internet access has been lost");
+
+        // log each active connection
+        Console.WriteLine("Connections active: ");
+        foreach (var item in e.ConnectionProfiles)
+        {
+            switch (item)
+            {
+                case ConnectionProfile.Bluetooth: 
+                    Console.WriteLine("Bluetooth"); 
+                    break;
+                case ConnectionProfile.Cellular: Console.WriteLine("Cell");
+                    break;
+                case ConnectionProfile.Ethernet: Console.WriteLine("Ethernet");
+                    break;
+                case ConnectionProfile.WiFi: Console.WriteLine("Wifi");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public void CheckConnectivity()
+    {
+        if (accessType == NetworkAccess.Internet)
+        {
+            // Connection to internet is available
+        }
+        if (profiles.Contains(ConnectionProfile.Cellular))
+        {
+            // Active Cellular connection
+        }
+        if (profiles.Contains(ConnectionProfile.WiFi))  
+        {
+            // Active WiFi conection
+        }
+    }
+}
