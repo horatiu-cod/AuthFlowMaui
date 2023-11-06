8300988e058896d7c46ff590ef862bc4aea34813
horatiu <horatiu.cod@gmail.com>
2023-11-06 06:05:04 +0200

    added LoginPageViewModel
    set BindingContext of LoginPage to LoginPageViewMode

---
+++ AuthFlowMaui/Pages/UserLogin/LoginPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AuthFlowMaui.Features.UserLogin;
+
+namespace AuthFlowMaui.Pages.UserLogin;
+
+public class LoginPageViewModel
+{
+    private readonly UserLoginViewModel _userLoginViewModel;
+
+    public LoginPageViewModel(UserLoginViewModel userLoginViewModel)
+    {
+        _userLoginViewModel = userLoginViewModel;
+    }
+}
