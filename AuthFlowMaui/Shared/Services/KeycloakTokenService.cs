82cb303bbcf32efca72ac5c92103694386ea4566
horatiu <horatiu.cod@gmail.com>
2023-11-07 06:18:08 +0200

    passed KeycloakSettings as metnod parameter

--- AuthFlowMaui/Shared/Services/KeycloakTokenService.cs
+++ AuthFlowMaui/Shared/Services/KeycloakTokenService.cs
@@ -18,37 +18,34 @@ public class KeycloakTokenService : IKeycloakTokenService
         _keycloakSettings = keycloakSettings;
     }
 
-    public async Task<KeycloakTokenResponseDto> GetUserTokenResponseAsync(KeycloakUserDtos keycloakUserDtos)
+    public async Task<KeycloakTokenResponseDto> GetUserTokenResponseAsync(KeycloakUserDtos keycloakUserDtos, KeycloakSettings keycloakSettings)
     {
-        using (var httpclient = _httpClientFactory.CreateClient())
-        {
+        var httpclient = _httpClientFactory.CreateClient();
             var keycloakTokenRequestDto = new KeycloakUserTokenRequestDto
             {
                 GrantType = KeycloakAccessTokenConst.GrantTypePassword,
-                ClientId = _keycloakSettings.ClientId,
-                ClientSecret = _keycloakSettings.ClientSecret,
+            ClientId = keycloakSettings.ClientId,
+            ClientSecret = keycloakSettings.ClientSecret,
                 Username = keycloakUserDtos.Username,
                 Password = keycloakUserDtos.Password
             };
             var tokenRequestBody = KeycloakTokenUtils.GetUserTokenRequestBody(keycloakTokenRequestDto);
-            var response = await httpclient.PostAsync($"{_keycloakSettings.BaseUrl}/token", tokenRequestBody);
+        var response = await httpclient.PostAsync($"{keycloakSettings.BaseUrl}/token", tokenRequestBody);
             var responseJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             var keycloakTokenResponseDto = JsonSerializer.Deserialize<KeycloakTokenResponseDto>(responseJson);
             return keycloakTokenResponseDto;
         }
-    }
-    public async Task<KeycloakTokenResponseDto> GetClientTokenResponseAsync()
+    public async Task<KeycloakTokenResponseDto> GetClientTokenResponseAsync(KeycloakSettings keycloakSettings)
     {
-        using (var httpclient = _httpClientFactory.CreateClient())
-        {
+        var httpClient = _httpClientFactory.CreateClient();
             var keycloakTokenRequestDto = new KeycloakClientTokenRequestDto
             {
-                GrantType = KeycloakAccessTokenConst.GrantTypePassword,
-                ClientId = _keycloakSettings.ClientId,
-                ClientSecret = _keycloakSettings.ClientSecret,
+            GrantType = KeycloakAccessTokenConst.GrantTypeCredentials,
+            ClientId = keycloakSettings.ClientId,
+            ClientSecret = keycloakSettings.ClientSecret,
             };
             var tokenRequestBody = KeycloakTokenUtils.GetClientTokenRequestBody(keycloakTokenRequestDto);
-            var response = await httpclient.PostAsync($"{_keycloakSettings.BaseUrl}/token", tokenRequestBody);
+        var response = await httpClient.PostAsync($"{keycloakSettings.BaseUrl}/token", tokenRequestBody);
             var responseJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             var keycloakTokenResponseDto = JsonSerializer.Deserialize<KeycloakTokenResponseDto>(responseJson);
             return keycloakTokenResponseDto;
