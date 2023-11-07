0019f239b4f28fe6b53b5b8e9da0a73feaa33bea
horatiu <horatiu.cod@gmail.com>
2023-11-07 06:11:00 +0200

    added KeyValuePair to GetUserTokenWithRefreshTokenRequestBody

--- AuthFlowMaui.Shared/Utils/KeycloakTokenUtils.cs
+++ AuthFlowMaui.Shared/Utils/KeycloakTokenUtils.cs
@@ -35,7 +35,8 @@ public static class KeycloakTokenUtils
         {
             new KeyValuePair<string, string>(KeycloakAccessTokenConst.GrantType, tokenRequestDto.GrantType),
             new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientId, tokenRequestDto.ClientId),
-            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientSecret, tokenRequestDto.ClientSecret)
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.ClientSecret, tokenRequestDto.ClientSecret),
+            new KeyValuePair<string, string>(KeycloakAccessTokenConst.RefreshToken, tokenRequestDto.RefreshToken)
         };
         return new FormUrlEncodedContent(keyValuePairs);
     }
