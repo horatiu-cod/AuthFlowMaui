08585786b0b9cf347e10967c630c0cf0bafe5741
horatiu <horatiu.cod@gmail.com>
2023-11-07 17:20:00 +0200

    added KeycloakRegisterUserDto

---
+++ AuthFlowMaui.Shared/Dtos/KeycloakRegisterUserDto.cs
@@ -0,0 +1,41 @@
+﻿using AuthFlowMaui.Shared.TokenDtos;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.Dtos;
+
+public class KeycloakRegisterUserDto
+{
+    [JsonPropertyName("firstName")]
+    public string? FirstName { get; set; }
+    [JsonPropertyName("lastName")]
+    public string? LastName { get; set;}
+    [JsonPropertyName("email")]
+    public string? Email { get; set;}
+    [JsonPropertyName("enabled")]
+    public bool? Enabled { get; set; }
+    [JsonPropertyName("credentials")]
+    public Credentials? Credentials { get; set; }
+    [JsonPropertyName("username")]
+    public string? UserName { get; set; }
+
+    public string ToJson() => JsonSerializer.Serialize(this);
+    public KeycloakRegisterUserDto? FromJson(string keycloakRegisterUserDto) =>
+        JsonSerializer.Deserialize<KeycloakRegisterUserDto>(keycloakRegisterUserDto, JsonSerializerOptions);
+    readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+}
+
+public class Credentials
+{
+    [JsonPropertyName("type")]
+    public string? Type { get; set;}
+    [JsonPropertyName("value")]
+    public string? Value { get; set; }
+    [JsonPropertyName("temporary")]
+    public bool Temporary { get; set; }
+}
