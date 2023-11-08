a854e9a08be079aadb239e37ec16c262f5eeca83
horatiu <horatiu.cod@gmail.com>
2023-11-08 15:46:33 +0200

    add RegisterKeycloakUser
    create named httpclient, add authentication header

---
+++ AuthFlowMaui/Shared/Services/IKeycloakApiService.cs
@@ -0,0 +1,10 @@
+﻿using AuthFlowMaui.Shared.Dtos;
+using AuthFlowMaui.Shared.Utils;
+
+namespace AuthFlowMaui.Shared.Services
+{
+    public interface IKeycloakApiService
+    {
+        Task<MethodResult> RegisterKeycloakUser(KeycloakRegisterUserDto keycloakRegisterUserDto, string token);
+    }
+}
---
+++ AuthFlowMaui/Shared/Services/KeycloakApiService.cs
@@ -0,0 +1,39 @@
+﻿using AuthFlowMaui.Shared.Dtos;
+using AuthFlowMaui.Shared.Utils;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace AuthFlowMaui.Shared.Services;
+
+public class KeycloakApiService : IKeycloakApiService
+{
+    private IHttpClientFactory _httpClientFactory;
+
+    public KeycloakApiService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<MethodResult> RegisterKeycloakUser(KeycloakRegisterUserDto keycloakRegisterUserDto, string token)
+    {
+        var httpClient = _httpClientFactory.CreateClient("maui-to-https-keycloak");
+        var RegisterUserBody = keycloakRegisterUserDto.ToJson();
+        var stringContent = new StringContent(RegisterUserBody, Encoding.UTF8, "application/json");
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var result = await httpClient.PostAsync("/admin/realms/dev/users", stringContent);
+
+        if (result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            return MethodResult.Fail("You are unauthorized");
+        }
+        else if (!result.IsSuccessStatusCode)
+        {
+            return MethodResult.Fail($"{result.StatusCode} {result.ReasonPhrase}");
+        }
+        else
+        {
+            return MethodResult.Success();
+        }
+    }
+}
