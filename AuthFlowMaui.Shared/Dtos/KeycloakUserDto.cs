adc1e6ce873d6673533db27d592542e4c6cdc87f
horatiu <horatiu.cod@gmail.com>
2023-11-12 09:56:44 +0200

    rename properties as naming policy camelcase

--- AuthFlowMaui.Shared/Dtos/KeycloakUserDto.cs
+++ AuthFlowMaui.Shared/Dtos/KeycloakUserDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace AuthFlowMaui.Shared.Dtos;
 
-public class KeycloakUserDtos
+public class KeycloakUserDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
 }
