ba14620194e4337abda293963b2ce01c4961809d
horatiu <horatiu.cod@gmail.com>
2023-11-13 16:59:22 +0200

    add IMauiInterop, implement MauiInterop
    helper class for interacting with MAUI in VM

---
+++ AuthFlowMaui/Shared/Utils/IMauiInterop.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AuthFlowMaui.Shared.Utils
+{
+    public interface IMauiInterop
+    {
+        Task<bool> ShowAlertWithActionAsync(string message, string accept, string cancel, string Title);
+        Task ShowErrorAlertAsync(string message, string Title = "Error");
+        Task ShowSuccessAlertAsync(string message, string Title = "Success");
+    }
+}
---
+++ AuthFlowMaui/Shared/Utils/MauiInterop.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthFlowMaui.Shared.Utils;
+
+public class MauiInterop : IMauiInterop
+{
+    public async Task ShowErrorAlertAsync(string message, string? Title = "Error") =>
+        await App.Current.MainPage.DisplayAlert(Title, message, "Ok");
+
+    public async Task ShowSuccessAlertAsync(string message, string? Title = "Success") =>
+        await App.Current.MainPage.DisplayAlert(Title, message, "Ok");
+
+    public async Task<bool> ShowAlertWithActionAsync(string message, string? accept, string? cancel, string? Title) =>
+        await App.Current.MainPage.DisplayAlert(Title, message, accept, cancel);
+
+}
