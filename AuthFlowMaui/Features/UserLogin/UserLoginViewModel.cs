3b0c7ac98aeb6f2d8b2eea6d89893e01f553340e
horatiu <horatiu.cod@gmail.com>
2023-11-15 18:53:28 +0200

    implement UerLogin logic

--- AuthFlowMaui/Features/UserLogin/UserLoginViewModel.cs
+++ AuthFlowMaui/Features/UserLogin/UserLoginViewModel.cs
@@ -1,11 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AuthFlowMaui.Pages;
+using AuthFlowMaui.Shared.Dtos;
+using AuthFlowMaui.Shared.Services;
+using AuthFlowMaui.Shared.Utils;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AuthFlowMaui.Features.UserLogin;
 
-public class UserLoginViewModel
+public partial class UserLoginViewModel : ObservableObject, IDisposable
 {
+    private readonly IKeycloakTokenService _keycloakTokenService;
+    private readonly IStorageService _storageService;
+    private readonly ITokenService _tokenService;
+    private readonly IConnectivityTest _connectivityTest;
+    private readonly IMauiInterop _mauiInterop;
+    private static readonly CancellationTokenSource s_tokenSource = new CancellationTokenSource();
+
+    public UserLoginViewModel(IKeycloakTokenService keycloakTokenService, IStorageService storageService, ITokenService tokenService, IConnectivityTest connectivityTest, IMauiInterop mauiInterop)
+    {
+        _keycloakTokenService = keycloakTokenService;
+        _storageService = storageService;
+        _tokenService = tokenService;
+        _connectivityTest = connectivityTest;
+        _mauiInterop = mauiInterop;
+    }
+
+    [ObservableProperty]
+    string _userName;
+    [ObservableProperty]
+    string _password;
+    [ObservableProperty]
+    bool _isBusy;
+
+    [RelayCommand]
+    private async Task LoginUser ()
+    {
+        var clientSettings = await _storageService.GetClientSecretAsync();
+        var user = new KeycloakUserDto
+        {
+            UserName = UserName,
+            Password = Password
+        };
+        if (_connectivityTest.CheckConnectivity())
+        {
+            try
+            {
+                s_tokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+                IsBusy = true;
+                var loginResult = await _keycloakTokenService.GetUserTokenResponseAsync(user, clientSettings.Data, s_tokenSource.Token);
+                IsBusy = false;
+                if (loginResult.IsSuccess)
+                {
+                    var storeResult = await _storageService.SetUserCredentialsAsync(loginResult.Data.ToJson());
+                    if (storeResult.IsSuccess)
+                    {
+                        await Toast.Make("You are logged in", CommunityToolkit.Maui.Core.ToastDuration.Short, 20).Show();
+                        await _mauiInterop.ShowSuccessAlertAsync(loginResult.Data.AccessToken);
+                        var state = DeviceInfo.Platform == DevicePlatform.Android ? $"//{nameof(MainPage)}" : $"{nameof(MainPage)}";
+                        await Shell.Current.GoToAsync(state);
+                        Dispose();
+                    }
+                    else
+                    {
+                        await _mauiInterop.ShowErrorAlertAsync("Credentials couldn't been stored");
+                    }
+                }
+                else
+                {
+                    await _mauiInterop.ShowErrorAlertAsync(loginResult.Error);
+                }
+
+            }
+            catch (OperationCanceledException ex)
+            {
+                IsBusy = false;
+                await _mauiInterop.ShowErrorAlertAsync($"Error: {ex.Message}", "Connection error");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        else
+        {
+            await _mauiInterop.ShowErrorAlertAsync("No network connection");
+        }
+    }
+    public void Dispose()
+    {
+        s_tokenSource.Cancel();
+        s_tokenSource.Dispose();
+    }
 }
