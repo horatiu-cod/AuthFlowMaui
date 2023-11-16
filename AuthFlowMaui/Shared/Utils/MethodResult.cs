0ae187d0571cbbf2db458cf1a0a29e757006bf15
horatiu <horatiu.cod@gmail.com>
2023-11-16 07:46:41 +0200

    remove #pragma warning disable CS8632

--- AuthFlowMaui/Shared/Utils/MethodResult.cs
+++ AuthFlowMaui/Shared/Utils/MethodResult.cs
@@ -1,13 +1,9 @@
 ﻿namespace AuthFlowMaui.Shared.Utils
 {
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     public readonly record struct MethodResult(bool IsSuccess, string? Error)
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     {
         public static MethodResult Success() => new(true, null);
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         public static MethodResult Fail(string? error) => new(false, error);
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     }
     public class MethodDataResult<TData>
     {
@@ -23,8 +19,6 @@
         public TData Data { get; set; }
 
         public static MethodDataResult<TData> Success(TData data) => new(true, null, data);
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         public static MethodDataResult<TData> Fail(string? error,TData? data) => new(false, error, default);
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     }
 }
