2e3be559159ef0115052c8f44d1bbd50d26c1a81
horatiu <horatiu.cod@gmail.com>
2023-11-17 13:07:22 +0200

    add KeycloakTokenValidationParametersDto

---
+++ AuthFlowMaui.Shared/TokenDtos/KeycloakTokenValidationParametersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthFlowMaui.Shared.TokenDtos;
+
+public class KeycloakTokenValidationParametersDto
+{
+    public string? ValidIssuer { get; set; }
+    public string? ValidAudience { get; set;}
+    public IEnumerable<string>? ValidAudiences { get; set; }
+}
