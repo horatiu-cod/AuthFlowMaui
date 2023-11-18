2d27b4d90b1d177612f57ea4e3d31fb82028c3b5
horatiu <horatiu.cod@gmail.com>
2023-11-18 13:40:29 +0200

    add propertires

--- AuthFlowMaui.Shared/TokenDtos/KeycloakTokenValidationParametersDto.cs
+++ AuthFlowMaui.Shared/TokenDtos/KeycloakTokenValidationParametersDto.cs
@@ -1,8 +1,14 @@
-﻿namespace AuthFlowMaui.Shared.TokenDtos;
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace AuthFlowMaui.Shared.TokenDtos;
 
 public class KeycloakTokenValidationParametersDto
 {
     public string? ValidIssuer { get; set; }
     public string? ValidAudience { get; set;}
     public IEnumerable<string>? ValidAudiences { get; set; }
+    public SecurityKey? IssuerSigningKey { get; set; }
+    public bool ValidateAudience { get; set; }
+    public bool ValidateIssuerSigningKey { get;set; }
+    public bool ValidateLifetime { get; set; }
 }
