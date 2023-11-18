7351d721ec103ef57d3a284016560aa6e39fb1eb
horatiu <horatiu.cod@gmail.com>
2023-11-18 14:43:54 +0200

    add ConnectivityTest check

--- AuthFlowMaui/Pages/AppStartUp/LoadingPageViewModel.cs
+++ AuthFlowMaui/Pages/AppStartUp/LoadingPageViewModel.cs
@@ -11,16 +11,18 @@ public partial class LoadingPageViewModel : ObservableObject
     private readonly IAuthService _authService;
     private readonly IStorageService _secureStorage;
     private readonly IMauiInterop _mauiInterop;
+    private readonly IConnectivityTest _connectivityTest;
     private static readonly CancellationTokenSource s_tokenSource = new CancellationTokenSource();
 
     public KeycloakSettingsViewModel KeycloakSettingsViewModel { get;}
 
-    public LoadingPageViewModel(KeycloakSettingsViewModel keycloakSettingsViewModel, IAuthService authService, IStorageService secureStorage, IMauiInterop mauiInterop)
+    public LoadingPageViewModel(KeycloakSettingsViewModel keycloakSettingsViewModel, IAuthService authService, IStorageService secureStorage, IMauiInterop mauiInterop, IConnectivityTest connectivityTest)
     {
         KeycloakSettingsViewModel = keycloakSettingsViewModel;
         _authService = authService;
         _secureStorage = secureStorage;
         _mauiInterop = mauiInterop;
+        _connectivityTest = connectivityTest;
     }
 
     [ObservableProperty]
@@ -45,44 +47,59 @@ public partial class LoadingPageViewModel : ObservableObject
         }
         else
         {
-            try
+            if (_connectivityTest.CheckConnectivity())
             {
-                IsBusy = true;
-                //await _secureStorage.RemoveUserCredentialsAsync();
-                s_tokenSource.CancelAfter(10000);
-                var response = await _authService.CheckIfIsAuthenticatedAsync(s_tokenSource.Token);
-                if (response.IsSuccess)
+                try
                 {
-                    // user is logged in
-                    // redirect to mainpage
-                    var state = _mauiInterop.SetState(nameof(MainPage));
-                    await Shell.Current.GoToAsync(state);
+                    IsBusy = true;
+                    //await _secureStorage.RemoveUserCredentialsAsync();
+                    s_tokenSource.CancelAfter(200000);
+                    var response = await _authService.CheckIfIsAuthenticatedAsync(s_tokenSource.Token);
+                    if (response.IsSuccess)
+                    {
+                        // user is logged in
+                        // redirect to mainpage
+                        var state = _mauiInterop.SetState(nameof(MainPage));
+                        await Shell.Current.GoToAsync(state);
+                    }
+                    else
+                    {
+                        // user is not logged in
+                        // redirect to loginpage
+                        IsBusy = false;
+                        await _mauiInterop.ShowErrorAlertAsync(response.Error);
+                        var state = _mauiInterop.SetState(nameof(LoginPage));
+                        s_tokenSource.Dispose();
+                        await Shell.Current.GoToAsync(state);
+                    }
                 }
-                else
+                catch (OperationCanceledException ex)
                 {
-                    // user is not logged in
-                    // redirect to loginpage
+                    var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("Connection error", ex.Message, null, "Cancel");
+                    if (!cancelAction)
+                    {
+                        var state = _mauiInterop.SetState(nameof(LoginPage));
+                        s_tokenSource.Dispose();
+                        await Shell.Current.GoToAsync(state);
+                    }
+                }
+                finally
+                {
                     IsBusy = false;
-                    await _mauiInterop.ShowErrorAlertAsync(response.Error);
-                    var state = _mauiInterop.SetState(nameof(LoginPage));
                     s_tokenSource.Dispose();
-                    await Shell.Current.GoToAsync(state);
                 }
             }
-            catch (OperationCanceledException ex)
-            {
-                var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("Connection error", ex.Message, null, "Cancel");
+            else 
+            { 
+                IsBusy = false;
+                var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("No network connection",null , "OK", "Error");
                 if (!cancelAction)
                 {
                     var state = _mauiInterop.SetState(nameof(LoginPage));
                     s_tokenSource.Dispose();
                     await Shell.Current.GoToAsync(state);
                 }
-            }
-            finally
-            {
-                IsBusy = false;
-                s_tokenSource.Dispose();
+
             }
         }
 
