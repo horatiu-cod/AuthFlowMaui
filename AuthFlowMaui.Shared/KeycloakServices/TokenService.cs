4cef55a83e4a8004cc6fa0f19811fda6b66614a9
horatiu <horatiu.cod@gmail.com>
2023-11-18 18:43:51 +0200

    remove ClaimsPrincipal ValidateToken method

--- AuthFlowMaui.Shared/KeycloakServices/TokenService.cs
+++ AuthFlowMaui.Shared/KeycloakServices/TokenService.cs
@@ -2,29 +2,10 @@
 using AuthFlowMaui.Shared.TokenDtos;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-
 namespace AuthFlowMaui.Shared.KeycloakServices;
 
 public class TokenService : ITokenService
 {
-    //public ClaimsPrincipal ValidateToken(string token)
-    //{
-    //    var tokenHandler = new JwtSecurityTokenHandler();
-    //    var validationParameter = new TokenValidationParameters
-    //    {
-    //        ValidateLifetime = true,
-    //    };
-    //    try
-    //    {
-    //        return tokenHandler.ValidateToken(token, validationParameter, out _);
-    //    }
-    //    catch (Exception)
-    //    {
-
-    //        return null;
-    //    }
-    //}
     public async Task<Result> ValidateTokenAsync(string token, KeycloakTokenValidationParametersDto keycloakTokenValidationParametersDto)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
