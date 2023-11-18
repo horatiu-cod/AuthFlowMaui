c84fa79251e65d368a7c3e497cd27e31b7ce7f0f
horatiu <horatiu.cod@gmail.com>
2023-11-18 14:45:50 +0200

    move ConnectivityChanged evethandler to separate class

---
+++ AuthFlowMaui/Shared/Utils/ConnectivityState.cs
@@ -0,0 +1,42 @@
+﻿namespace AuthFlowMaui.Shared.Utils;
+
+public class ConnectivityState
+{
+    public ConnectivityState() => Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    ~ConnectivityState() => Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+
+
+    private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+    {
+        if (e.NetworkAccess == NetworkAccess.ConstrainedInternet)
+            Console.WriteLine("Internet access is available but is limited");
+        else if (e.NetworkAccess == NetworkAccess.Local)
+            Console.WriteLine("Only local internet is available");
+        else if (e.NetworkAccess != NetworkAccess.Internet)
+            Console.WriteLine("Internet access has been lost");
+
+        // log each active connection
+        Console.WriteLine("Connections active: ");
+        foreach (var item in e.ConnectionProfiles)
+        {
+            switch (item)
+            {
+                case ConnectionProfile.Bluetooth:
+                    Console.WriteLine("Bluetooth");
+                    break;
+                case ConnectionProfile.Cellular:
+                    Console.WriteLine("Cell");
+                    break;
+                case ConnectionProfile.Ethernet:
+                    Console.WriteLine("Ethernet");
+                    break;
+                case ConnectionProfile.WiFi:
+                    Console.WriteLine("WiFi");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
--- AuthFlowMaui/Shared/Utils/ConnectivityTest.cs
+++ AuthFlowMaui/Shared/Utils/ConnectivityTest.cs
@@ -9,44 +9,10 @@
 // <users-permission android: name="android:permission.ACCESS_NETWORK_STATE />
 public class ConnectivityTest : IConnectivityTest
 {
-    public ConnectivityTest() => Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
-    ~ConnectivityTest() => Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
 
     IEnumerable<ConnectionProfile> profiles = Connectivity.Current.ConnectionProfiles;
     NetworkAccess accessType = Connectivity.Current.NetworkAccess;
 
-    private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
-    {
-        if (e.NetworkAccess == NetworkAccess.ConstrainedInternet)
-            Console.WriteLine("Internet access is available but is limited");
-        else if (e.NetworkAccess == NetworkAccess.Local)
-            Console.WriteLine("Only local internet is available");
-        else if (e.NetworkAccess != NetworkAccess.Internet)
-            Console.WriteLine("Internet access has been lost");
-
-        // log each active connection
-        Console.WriteLine("Connections active: ");
-        foreach (var item in e.ConnectionProfiles)
-        {
-            switch (item)
-            {
-                case ConnectionProfile.Bluetooth:
-                    Console.WriteLine("Bluetooth");
-                    break;
-                case ConnectionProfile.Cellular:
-                    Console.WriteLine("Cell");
-                    break;
-                case ConnectionProfile.Ethernet:
-                    Console.WriteLine("Ethernet");
-                    break;
-                case ConnectionProfile.WiFi:
-                    Console.WriteLine("WiFi");
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
     public bool CheckConnectivity()
     {
         if (accessType == NetworkAccess.Internet)
