d0bfbb532d8e41e5bef213601add5a47272bb861
horatiu <horatiu.cod@gmail.com>
2023-11-19 21:51:31 +0200

    refactored result record, passed HttpStatusCode

--- AuthFlowMaui.Shared/KeycloakUtils/Results.cs
+++ AuthFlowMaui.Shared/KeycloakUtils/Results.cs
@@ -2,10 +2,12 @@
 
 namespace AuthFlowMaui.Shared.KeycloakUtils
 {
-    public readonly record struct Result(bool IsSuccess, string? Error)
+    public readonly record struct Result(bool IsSuccess, string? Error, HttpStatusCode? HttpStatus)
     {
-        public static Result Success() => new(true, null);
-        public static Result Fail(string? error) => new(false, error);
+        public static Result Success() => new(true, null, null);
+        public static Result Success(HttpStatusCode? httpStatus) => new(true, null, httpStatus);
+        public static Result Fail(string? error) => new(false, error, null);
+        public static Result Fail(string? error, HttpStatusCode? httpStatus) => new(false, error, httpStatus);
     }
 
     public record struct Result<TData>(TData? Content, bool IsSuccess, HttpStatusCode? HttpMessage, string? Error)
