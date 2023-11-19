e407538af8f0118e25332014537eca174b91c719
horatiu <horatiu.cod@gmail.com>
2023-11-19 17:30:44 +0200

    add generic record Result

--- AuthFlowMaui.Shared/KeycloakUtils/Results.cs
+++ AuthFlowMaui.Shared/KeycloakUtils/Results.cs
@@ -1,10 +1,22 @@
-﻿namespace AuthFlowMaui.Shared.KeycloakUtils
+﻿using System.Net;
+
+namespace AuthFlowMaui.Shared.KeycloakUtils
 {
     public readonly record struct Result(bool IsSuccess, string? Error)
     {
         public static Result Success() => new(true, null);
         public static Result Fail(string? error) => new(false, error);
     }
+
+    public record struct Result<TData>(TData? Content, bool IsSuccess, HttpStatusCode? HttpMessage, string? Error)
+    {
+        public static Result<TData> Success(TData content) => new(content, true, null, null);
+        public static Result<TData> Success(TData content, HttpStatusCode? message) => new(content, true, message, null);
+        public static Result<TData> Fail(HttpStatusCode? HttpMessage) => new(default, true, HttpMessage, null);
+        public static Result<TData> Fail(TData? content, HttpStatusCode? HttpMessage) => new(default, true, HttpMessage, null);
+        public static Result<TData> Fail(TData? content, string? Error) => new(default, true, null, Error);
+    }
+
     public class DataResult<TData>
     {
         public DataResult(bool isSuccess, string? error, TData? data)
@@ -21,4 +33,6 @@
         public static DataResult<TData> Success(TData data) => new(true, null, data);
         public static DataResult<TData> Fail(string? error,TData? data) => new(false, error, default);
     }
+
+    public record HttpStatus(HttpStatusCode StatusCode, string? Message);
 }
