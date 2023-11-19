53136f0c86411dc6420c83a1f3a7613803bd5549
horatiu <horatiu.cod@gmail.com>
2023-11-19 21:46:37 +0200

    added

---
+++ AuthFlowMaui.Shared/TokenDtos/KeycloakUserLogoutWithRefreshTokenRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.TokenDtos;
+
+public class KeycloakUserLogoutWithRefreshTokenRequestDto
+{
+    public string? ClientId { get; set; }
+    [JsonPropertyName("client_secret")]
+    public string? ClientSecret { get; set; }
+    [JsonPropertyName("refresh_token")]
+    public string? RefreshToken { get; set; }
+
+}
