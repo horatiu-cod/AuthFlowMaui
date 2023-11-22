2c437664346e60fb6c7e4b04e1c419d13f7bf775
horatiu <horatiu.cod@gmail.com>
2023-11-22 17:39:26 +0200

    move to Client folder

---
+++ AuthFlowMaui.Shared/Extensions/Client/ClientExtensions.cs
@@ -0,0 +1,77 @@
+﻿using AuthFlowMaui.Shared.Constants;
+using AuthFlowMaui.Shared.Dtos;
+using AuthFlowMaui.Shared.KeycloakSettings;
+using AuthFlowMaui.Shared.KeycloakUtils;
+using AuthFlowMaui.Shared.TokenDtos;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace AuthFlowMaui.Shared.Extensions.Client;
+
+public static class ClientExtensions
+{
+    public async static Task<HttpResponseMessage> LoginGrantTypePassword(this HttpClient httpClient, string userName, string password, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
+    {
+        var requestDto = new KeycloakUserTokenRequestDto
+        {
+            GrantType = KeycloakAccessTokenConst.GrantTypePassword,
+            ClientId = keycloakSettings.ClientId,
+            ClientSecret = keycloakSettings.ClientSecret,
+            Username = userName,
+            Password = password
+        };
+        var requestBody = KeycloakTokenUtils.GetUserTokenRequestBody(requestDto);
+        var response = await httpClient.PostAsync($"/realms/dev/protocol/openid-connect/token", requestBody, cancellationToken);
+        return response;
+    }
+    public async static Task<HttpResponseMessage> LoginGrantTypeRefreshToken(this HttpClient httpClient, string refreshToken, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
+    {
+        var requestDto = new KeycloakUserTokenWithRefreshTokenRequestDto()
+        {
+            GrantType = KeycloakAccessTokenConst.GrantTypePassword,
+            ClientId = keycloakSettings.ClientId,
+            ClientSecret = keycloakSettings.ClientSecret,
+            RefreshToken = refreshToken
+        };
+        var requestBody = KeycloakTokenUtils.GetUserTokenWithRefreshTokenRequestBody(requestDto);
+        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/token", requestBody, cancellationToken);
+        return response;
+    }
+    public static async Task<HttpResponseMessage> Logout(this HttpClient httpClient, KeycloakClientSettings keycloakSettings, string refreshToken, CancellationToken cancellationToken)
+    {
+        var requestDto = new KeycloakUserLogoutWithRefreshTokenRequestDto
+        {
+            ClientId = keycloakSettings.ClientId,
+            ClientSecret = keycloakSettings.ClientSecret,
+            RefreshToken = refreshToken
+        };
+        var requestBody = KeycloakTokenUtils.LogoutUserWithRefreshTokenRequestBody(requestDto);
+        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/logout", requestBody, cancellationToken);
+        return response;
+    }
+    public static async Task<HttpResponseMessage> Register(this HttpClient httpClient, KeycloakRegisterUserDto keycloakRegisterUserDto, KeycloakClientSettings clientSettings, CancellationToken cancellationToken)
+    {
+        var keycloakTokenRequestDto = new KeycloakClientRequestDto
+        {
+            GrantType = KeycloakAccessTokenConst.GrantTypeCredentials,
+            ClientId = clientSettings.ClientId,
+            ClientSecret = clientSettings.ClientSecret,
+        };
+        var tokenRequestBody = KeycloakTokenUtils.GetClientTokenRequestBody(keycloakTokenRequestDto);
+        var response = await httpClient.PostAsync($"/realms/{clientSettings.Realm}/protocol/openid-connect//token", tokenRequestBody, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+            return response;
+        var contentRaw = await response.Content.ReadAsStringAsync();
+        var content = JsonSerializer.Deserialize<KeycloakTokenResponseDto>(contentRaw);
+
+        var RegisterUserBody = keycloakRegisterUserDto.ToJson();
+        var stringContent = new StringContent(RegisterUserBody, Encoding.UTF8, "application/json");
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", content.AccessToken);
+
+        var result = await httpClient.PostAsync($"/admin/realms/{clientSettings.Realm}/users", stringContent, cancellationToken);
+        return result;
+    }
+    //public static async Task<Result> UpdateUser() { }
+    //public static async Task<Result> DeleteUser() { }
+}
