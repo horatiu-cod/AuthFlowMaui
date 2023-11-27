6068705f27c82ae2a2041f194e153e72430a43cb
horatiu <horatiu.cod@gmail.com>
2023-11-27 07:17:04 +0200

    add properties

--- AuthFlowMaui.Shared/Dtos/KeycloakUserDto.cs
+++ AuthFlowMaui.Shared/Dtos/KeycloakUserDto.cs
@@ -1,7 +1,24 @@
-﻿namespace AuthFlowMaui.Shared.Dtos;
+﻿using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.Dtos;
 
 public class KeycloakUserDto
 {
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+    [JsonPropertyName("username")]
     public string? UserName { get; set; }
+    [JsonPropertyName("password")]
     public string? Password { get; set; }
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+    [JsonPropertyName("firstName")]
+    public string? FirstName { get; set; }
+    [JsonPropertyName("lastName")]
+    public string? LastName { get; set; }
+    [JsonPropertyName("emailVerified")]
+    public bool IsEmailVerified { get; set; }
+    [JsonPropertyName("enabled")]
+    public bool IsEnabled { get; set; }
+
 }
