6c6a1db7a1382e4692983d73b72e5f10a594820a
horatiu <horatiu.cod@gmail.com>
2023-11-27 07:16:34 +0200

    add GetKeycloakUser method

--- AuthFlowMaui.Shared/KeycloakServices/KeycloakApiService.cs
+++ AuthFlowMaui.Shared/KeycloakServices/KeycloakApiService.cs
@@ -4,9 +4,11 @@ using System.Net.Http.Headers;
 using AuthFlowMaui.Shared.Dtos;
 using AuthFlowMaui.Shared.KeycloakUtils;
 using AuthFlowMaui.Shared.KeycloakSettings;
+using System.Net.Http.Json;
 
 namespace AuthFlowMaui.Shared.KeycloakServices;
-
+#pragma warning disable
+#nullable enable
 public class KeycloakApiService : IKeycloakApiService
 {
     private IHttpClientFactory _httpClientFactory;
@@ -47,6 +49,36 @@ public class KeycloakApiService : IKeycloakApiService
         else
         {
             return Result.Success(result.StatusCode);
+        }
+    }
+    public async Task<Result<KeycloakUserDto>> GetKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, string httpClientName, CancellationToken cancellationToken)
+    {
+        var httpClient = _httpClientFactory.CreateClient(httpClientName);
+
+        var client = await _keycloakTokenService.GetClientTokenResponseAsync(clientSettings, httpClientName, cancellationToken);
+        if (!client.IsSuccess)
+            return Result<KeycloakUserDto>.Fail(client.HttpStatus, null, $"{client.Error}, Error from GetClientTokenResponseAsync passed to RegisterKeycloakUser in KeycloakApiService");
+
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", client.Content.AccessToken);
+
+        var result = await httpClient.GetAsync($"/admin/realms/{clientSettings.Realm}/users/{keycloakUserDto.Id}", cancellationToken);
+
+        if (result.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            return Result<KeycloakUserDto>.Fail(result.StatusCode, null,$"{result.StatusCode} {result.ReasonPhrase} from RegisterKeycloakUser");
+        }
+        else if (!result.IsSuccessStatusCode)
+        {
+            return Result<KeycloakUserDto>.Fail(result.StatusCode, null, $"{result.StatusCode} {result.ReasonPhrase} from RegisterKeycloakUser");
+        }
+        else if (result.StatusCode != HttpStatusCode.Created)
+        {
+            return Result<KeycloakUserDto>.Fail(result.StatusCode, null, $"{result.StatusCode} {result.ReasonPhrase} from RegisterKeycloakUser");
+        }
+        else
+        {
+            keycloakUserDto = await result.Content.ReadFromJsonAsync<KeycloakUserDto>(cancellationToken); 
+            return Result<KeycloakUserDto>.Success(keycloakUserDto,result.StatusCode);
         }
     }
 }
