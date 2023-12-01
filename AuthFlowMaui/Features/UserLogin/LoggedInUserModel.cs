088bd1459fa261e70bbd57618e157130606c72a6
horatiu <horatiu.cod@gmail.com>
2023-12-01 22:57:59 +0200

    add LoggedInUserModel

---
+++ AuthFlowMaui/Features/UserLogin/LoggedInUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Features.UserLogin;
+
+public record struct LoggedInUserModel
+{
+    [Required]
+    [JsonPropertyName("username")]
+    public string UserName { get; set; }
+    [Required]
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+
+    public  string ToJson() =>
+        JsonSerializer.Serialize(this);
+    public LoggedInUserModel FromJson(string user) =>
+        JsonSerializer.Deserialize<LoggedInUserModel>(user);
+}
