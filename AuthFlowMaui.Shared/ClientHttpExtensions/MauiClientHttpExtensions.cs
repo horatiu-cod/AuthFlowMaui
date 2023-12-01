48de582c654acb69180eaacb21675dd0f733e76c
horatiu <horatiu.cod@gmail.com>
2023-12-01 17:33:18 +0200

    rename to MauiClientHttpExtensions

--- AuthFlowMaui.Shared/ClientHttpExtensions/MauiClientHttpExtensions.cs
+++ AuthFlowMaui.Shared/ClientHttpExtensions/MauiClientHttpExtensions.cs
@@ -1,15 +1,11 @@
 ﻿using AuthFlowMaui.Shared.Constants;
-using AuthFlowMaui.Shared.Dtos;
 using AuthFlowMaui.Shared.KeycloakSettings;
 using AuthFlowMaui.Shared.KeycloakUtils;
 using AuthFlowMaui.Shared.TokenDtos;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 
 namespace AuthFlowMaui.Shared.ClientHttpExtensions;
 
-public static class ClientExtensions
+public static class MauiClientHttpExtensions
 {
     public async static Task<HttpResponseMessage> LoginGrantTypePassword(this HttpClient httpClient, string userName, string password, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
     {
@@ -22,7 +18,7 @@ public static class ClientExtensions
             Password = password
         };
         var requestBody = KeycloakTokenUtils.GetUserTokenRequestBody(requestDto);
-        var response = await httpClient.PostAsync($"/realms/dev/protocol/openid-connect/token", requestBody, cancellationToken);
+        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/token", requestBody, cancellationToken);
         return response;
     }
     public async static Task<HttpResponseMessage> LoginGrantTypeRefreshToken(this HttpClient httpClient, string refreshToken, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
@@ -50,28 +46,28 @@ public static class ClientExtensions
         var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/logout", requestBody, cancellationToken);
         return response;
     }
-    public static async Task<HttpResponseMessage> Register(this HttpClient httpClient, KeycloakRegisterUserDto keycloakRegisterUserDto, KeycloakClientSettings clientSettings, CancellationToken cancellationToken)
-    {
-        var keycloakTokenRequestDto = new KeycloakClientRequestDto
-        {
-            GrantType = KeycloakAccessTokenConst.GrantTypeCredentials,
-            ClientId = clientSettings.ClientId,
-            ClientSecret = clientSettings.ClientSecret,
-        };
-        var tokenRequestBody = KeycloakTokenUtils.GetClientTokenRequestBody(keycloakTokenRequestDto);
-        var response = await httpClient.PostAsync($"/realms/{clientSettings.Realm}/protocol/openid-connect//token", tokenRequestBody, cancellationToken);
-        if (!response.IsSuccessStatusCode)
-            return response;
-        var contentRaw = await response.Content.ReadAsStringAsync();
-        var content = JsonSerializer.Deserialize<KeycloakTokenResponseDto>(contentRaw);
+    //public static async Task<HttpResponseMessage> Register(this HttpClient httpClient, KeycloakRegisterUserDto keycloakRegisterUserDto, KeycloakClientSettings clientSettings, CancellationToken cancellationToken)
+    //{
+    //    var keycloakTokenRequestDto = new KeycloakClientRequestDto
+    //    {
+    //        GrantType = KeycloakAccessTokenConst.GrantTypeCredentials,
+    //        ClientId = clientSettings.ClientId,
+    //        ClientSecret = clientSettings.ClientSecret,
+    //    };
+    //    var tokenRequestBody = KeycloakTokenUtils.GetClientTokenRequestBody(keycloakTokenRequestDto);
+    //    var response = await httpClient.PostAsync($"/realms/{clientSettings.Realm}/protocol/openid-connect//token", tokenRequestBody, cancellationToken);
+    //    if (!response.IsSuccessStatusCode)
+    //        return response;
+    //    var contentRaw = await response.Content.ReadAsStringAsync();
+    //    var content = JsonSerializer.Deserialize<KeycloakTokenResponseDto>(contentRaw);
 
-        var RegisterUserBody = keycloakRegisterUserDto.ToJson();
-        var stringContent = new StringContent(RegisterUserBody, Encoding.UTF8, "application/json");
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", content.AccessToken);
+    //    var RegisterUserBody = keycloakRegisterUserDto.ToJson();
+    //    var stringContent = new StringContent(RegisterUserBody, Encoding.UTF8, "application/json");
+    //    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", content.AccessToken);
 
-        var result = await httpClient.PostAsync($"/admin/realms/{clientSettings.Realm}/users", stringContent, cancellationToken);
-        return result;
-    }
+    //    var result = await httpClient.PostAsync($"/admin/realms/{clientSettings.Realm}/users", stringContent, cancellationToken);
+    //    return result;
+    //}
     //public static async Task<Result> UpdateUser() { }
     //public static async Task<Result> DeleteUser() { }
 }
