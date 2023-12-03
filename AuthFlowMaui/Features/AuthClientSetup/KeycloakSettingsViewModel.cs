32d1f40181e878be2ba57dbbeba8d2871646d9dd
horatiu <horatiu.cod@gmail.com>
2023-12-03 20:01:18 +0200

    add services, checked connectivity, ger realm keys

--- AuthFlowMaui/Features/AuthClientSetup/KeycloakSettingsViewModel.cs
+++ AuthFlowMaui/Features/AuthClientSetup/KeycloakSettingsViewModel.cs
@@ -1,19 +1,26 @@
 ﻿using AuthFlowMaui.Shared.KeycloakSettings;
-using AuthFlowMaui.Pages;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using AuthFlowMaui.Pages.UserLogin;
 using AuthFlowMaui.Shared.Services;
+using AuthFlowMaui.Shared.Utils;
+using AuthFlowMaui.Pages.AppStartUp;
 
 namespace AuthFlowMaui.Features.AuthClientSetup;
 
 public partial class KeycloakSettingsViewModel : ObservableObject
 {
     private readonly IStorageService _storageService;
+    private readonly IMauiInterop _mauiInterop;
+    private readonly IConnectivityTest _connectivityTest;
+    private readonly ICertsService _certsService;
+    private static readonly CancellationTokenSource s_tokenSource = new CancellationTokenSource();
 
-    public KeycloakSettingsViewModel(IStorageService storageService)
+    public KeycloakSettingsViewModel(IStorageService storageService, IMauiInterop mauiInterop, IConnectivityTest connectivityTest, ICertsService certsService)
     {
         _storageService = storageService;
+        _mauiInterop = mauiInterop;
+        _connectivityTest = connectivityTest;
+        _certsService = certsService;
     }
 
     [ObservableProperty]
@@ -33,12 +40,45 @@ public partial class KeycloakSettingsViewModel : ObservableObject
             ClientSecret = this.ClientSecret,
         };
         var result = await _storageService.SetClientSecretAsync(secretSettings.ToJson());
-        if (result.IsSuccess)
-        {
-            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        if (!result.IsSuccess)
+        { 
+            var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("Big Error", null, "Cancel", "Error");
+            if (!cancelAction)
+            {
+                App.Current.Quit();
+            }
         }
-        else { 
-            await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+        if (_connectivityTest.CheckConnectivity())
+        {
+            IsBusy = true;
+            s_tokenSource.CancelAfter(20000);
+            var realmKey = await _certsService.GetRealmCertsAsync(s_tokenSource.Token);
+            s_tokenSource.TryReset();
+            if (realmKey.IsSuccess) 
+            { 
+                IsBusy = false;
+                var state = _mauiInterop.SetState(nameof(LoadingPage));
+                await Shell.Current.GoToAsync($"///{state}");
+                s_tokenSource.Dispose();
+            }
+            else
+            {
+                IsBusy = false;
+                var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("No network connection", null, "Cancel", "Error");
+                if (!cancelAction)
+                {
+                    App.Current.Quit();
+                }
+            }
+        }
+        else
+        {
+            IsBusy = false;
+            var cancelAction = await _mauiInterop.ShowAlertWithActionAsync("No network connection", null, "Cancel", "Error");
+            if(!cancelAction)
+            {
+                App.Current.Quit();
+            }
         }
         IsBusy = false;
     }
