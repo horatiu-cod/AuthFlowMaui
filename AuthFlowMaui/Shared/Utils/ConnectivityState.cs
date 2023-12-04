97fb79738770a0ad3a5d18fa9e7de1ce94d498d3
horatiu <horatiu.cod@gmail.com>
2023-12-04 15:52:47 +0200

    add toast

--- AuthFlowMaui/Shared/Utils/ConnectivityState.cs
+++ AuthFlowMaui/Shared/Utils/ConnectivityState.cs
@@ -1,37 +1,41 @@
-﻿namespace AuthFlowMaui.Shared.Utils;
+﻿
+namespace AuthFlowMaui.Shared.Utils;
 
 public class ConnectivityState
 {
     public ConnectivityState() => Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
     ~ConnectivityState() => Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
 
+    public event EventHandler<ConnectivityChangedEventArgs> ConnectivityChanged;
 
     private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
     {
+        var mauiInterop = new MauiInterop();
         if (e.NetworkAccess == NetworkAccess.ConstrainedInternet)
-            Console.WriteLine("Internet access is available but is limited");
+            _ = mauiInterop.ShowToastAsync("Internet access is available but is limited", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
         else if (e.NetworkAccess == NetworkAccess.Local)
-            Console.WriteLine("Only local internet is available");
+            _ = mauiInterop.ShowToastAsync("Only local internet is available", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
         else if (e.NetworkAccess != NetworkAccess.Internet)
-            Console.WriteLine("Internet access has been lost");
+            _ = mauiInterop.ShowToastAsync("Internet access has been lost", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
 
         // log each active connection
-        Console.WriteLine("Connections active: ");
+        _=mauiInterop.ShowToastAsync("Connections active: ", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
+
         foreach (var item in e.ConnectionProfiles)
         {
             switch (item)
             {
                 case ConnectionProfile.Bluetooth:
-                    Console.WriteLine("Bluetooth");
+                    _ = mauiInterop.ShowToastAsync("Connections active: bluetooth", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
                     break;
                 case ConnectionProfile.Cellular:
-                    Console.WriteLine("Cell");
+                    _ = mauiInterop.ShowToastAsync("Connections active: cell", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
                     break;
                 case ConnectionProfile.Ethernet:
-                    Console.WriteLine("Ethernet");
+                    _ = mauiInterop.ShowToastAsync("Connections active: ethernet", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
                     break;
                 case ConnectionProfile.WiFi:
-                    Console.WriteLine("WiFi");
+                    _ = mauiInterop.ShowToastAsync("Connections active: wifi", CommunityToolkit.Maui.Core.ToastDuration.Long, 12);
                     break;
                 default:
                     break;
