11cf294a3c5a9ed2f114135addd9a391ee97dd60
horatiu <horatiu.cod@gmail.com>
2023-12-04 22:59:09 +0200

    change return type to Result

--- AuthFlowMaui.Shared/ClientHttpExtensions/MauiClientHttpExtensions.cs
+++ AuthFlowMaui.Shared/ClientHttpExtensions/MauiClientHttpExtensions.cs
@@ -1,13 +1,15 @@
 ﻿using AuthFlowMaui.Shared.Constants;
+using AuthFlowMaui.Shared.Dtos;
 using AuthFlowMaui.Shared.KeycloakSettings;
 using AuthFlowMaui.Shared.KeycloakUtils;
 using AuthFlowMaui.Shared.TokenDtos;
+using System.Net.Http.Json;
 
 namespace AuthFlowMaui.Shared.ClientHttpExtensions;
 
 public static class MauiClientHttpExtensions
 {
-    public async static Task<HttpResponseMessage> LoginGrantTypePassword(this HttpClient httpClient, string userName, string password, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
+    public async static Task<Result<KeycloakTokenResponseDto>> LoginGrantTypePassword(this HttpClient httpClient, string userName, string password, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken = default)
     {
         var requestDto = new KeycloakUserTokenRequestDto
         {
@@ -18,10 +20,15 @@ public static class MauiClientHttpExtensions
             Password = password
         };
         var requestBody = KeycloakTokenUtils.GetUserTokenRequestBody(requestDto);
-        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/token", requestBody, cancellationToken);
-        return response;
+        var response = await httpClient.PostAsync($"{keycloakSettings.RealmUrl}/token", requestBody, cancellationToken);
+        var keycloakTokenResponseDto = await response.Content.ReadFromJsonAsync<KeycloakTokenResponseDto>();
+        if (!response.IsSuccessStatusCode)
+        {
+            return Result <KeycloakTokenResponseDto>.Fail(response.StatusCode, $"{response.ReasonPhrase}");
+        }
+        return Result<KeycloakTokenResponseDto>.Success(keycloakTokenResponseDto!, response.StatusCode);
     }
-    public async static Task<HttpResponseMessage> LoginGrantTypeRefreshToken(this HttpClient httpClient, string refreshToken, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
+    public async static Task<Result<KeycloakTokenResponseDto>> RefreshToken(this HttpClient httpClient, string refreshToken, KeycloakClientSettings keycloakSettings, CancellationToken cancellationToken)
     {
         var requestDto = new KeycloakUserTokenWithRefreshTokenRequestDto()
         {
@@ -31,10 +38,15 @@ public static class MauiClientHttpExtensions
             RefreshToken = refreshToken
         };
         var requestBody = KeycloakTokenUtils.GetUserTokenWithRefreshTokenRequestBody(requestDto);
-        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/token", requestBody, cancellationToken);
-        return response;
+        var response = await httpClient.PostAsync($"{keycloakSettings.RealmUrl}token", requestBody, cancellationToken);
+        var keycloakTokenResponseDto = await response.Content.ReadFromJsonAsync<KeycloakTokenResponseDto>();
+        if (!response.IsSuccessStatusCode)
+        {
+            return Result<KeycloakTokenResponseDto>.Fail(response.StatusCode, $"{response.ReasonPhrase}");
+        }
+        return Result<KeycloakTokenResponseDto>.Success(keycloakTokenResponseDto!, response.StatusCode);
     }
-    public static async Task<HttpResponseMessage> Logout(this HttpClient httpClient, KeycloakClientSettings keycloakSettings, string refreshToken, CancellationToken cancellationToken)
+    public static async Task<Result> Logout(this HttpClient httpClient, KeycloakClientSettings keycloakSettings, string refreshToken, CancellationToken cancellationToken)
     {
         var requestDto = new KeycloakUserLogoutWithRefreshTokenRequestDto
         {
@@ -43,8 +55,32 @@ public static class MauiClientHttpExtensions
             RefreshToken = refreshToken
         };
         var requestBody = KeycloakTokenUtils.LogoutUserWithRefreshTokenRequestBody(requestDto);
-        var response = await httpClient.PostAsync($"/realms/{keycloakSettings.Realm}/protocol/openid-connect/logout", requestBody, cancellationToken);
-        return response;
+        var response = await httpClient.PostAsync($"{keycloakSettings.RealmUrl}/logout", requestBody, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            return Result.Fail( $"{response.RequestMessage} {response.ReasonPhrase}", response.StatusCode);
+        }
+        return Result.Success(response.StatusCode);
+    }
+    public static async Task<Result<KeycloakKeysDto>> GetRealmKeysAsync(this HttpClient httpClient,string url, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var response = await httpClient.GetAsync($"{url}/certs", cancellationToken);
+            if (response.IsSuccessStatusCode)
+            {
+                var keycloakKeyResponseDto = await response.Content.ReadFromJsonAsync<KeycloakKeysDto>();
+                return Result<KeycloakKeysDto>.Success(keycloakKeyResponseDto);
+            }
+            else
+            {
+                return Result<KeycloakKeysDto>.Fail(response.StatusCode, $"{response.StatusCode} {response.ReasonPhrase} from GetClientCertsResponseAsync");
+            }
+        }
+        catch (Exception ex)
+        {
+            return Result<KeycloakKeysDto>.Fail($"{ex.Message} exception from from GetClientCertsResponseAsync");
+        }
     }
     //public static async Task<HttpResponseMessage> Register(this HttpClient httpClient, KeycloakRegisterUserDto keycloakRegisterUserDto, KeycloakClientSettings clientSettings, CancellationToken cancellationToken)
     //{
