67baf6561a57d325b8646549e9b6e4590dcd7445
horatiu <horatiu.cod@gmail.com>
2023-12-05 19:53:15 +0200

    implement ClientHttpExtensions

--- AuthFlowMaui/Pages/MainPage.xaml.cs
+++ AuthFlowMaui/Pages/MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using AuthFlowMaui.Constants;
-using AuthFlowMaui.Shared.KeycloakServices;
+using AuthFlowMaui.Shared.ClientHttpExtensions;
 using AuthFlowMaui.Shared.Services;
 using System.IdentityModel.Tokens.Jwt;
 
@@ -7,19 +7,20 @@ namespace AuthFlowMaui.Pages
 {
     public partial class MainPage : ContentPage
     {
-        private readonly IKeycloakTokenService _keycloakTokenService;
+        private readonly IHttpClientFactory _httpClientFactory;
         private readonly IStorageService _storageService;
         static readonly CancellationTokenSource s_tokenSource = new CancellationTokenSource();
-        public MainPage(IKeycloakTokenService keycloakTokenService, IStorageService storageService)
+        public MainPage(IStorageService storageService, IHttpClientFactory httpClientFactory)
         {
             InitializeComponent();
-            _keycloakTokenService = keycloakTokenService;
             _storageService = storageService;
+            _httpClientFactory = httpClientFactory;
         }
 
         private async void OnCounterClicked(object sender, EventArgs e)
         {
             var httpClientName = RealmConstants.HttpClientName;
+            var httpClient = _httpClientFactory.CreateClient(httpClientName);
             var clientSettings = await _storageService.GetClientSecretAsync();
             if (clientSettings.IsSuccess) 
             {
@@ -28,27 +29,25 @@ namespace AuthFlowMaui.Pages
                 try
                 {
                     s_tokenSource.CancelAfter(13500);
-                    var response = await _keycloakTokenService.GetClientTokenResponseAsync(keycloakSettings, httpClientName, s_tokenSource.Token);
+                    var response = await httpClient.GetClientTokenAsync(keycloakSettings, s_tokenSource.Token);
                     s_tokenSource.TryReset();
                     if (response.IsSuccess)
                     {
                         var rawToken = response.Content.AccessToken;
-                        //var userName = await response.Data.HttpResponse.Content.ReadAsStringAsync();
                         var handler = new JwtSecurityTokenHandler();
                         var token = handler.ReadJwtToken(rawToken);
                         var claim = token.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
-                        //var res = response.;
                         apiResponse.Text = $"{claim} merge";
                     }
                     else
                     {
-                        apiResponse.Text = $"{ response.Error} passed from GetClientTokenResponseAsync to OnCounterClicked in MainPage.xaml.cs";
+                        apiResponse.Text = $"{ response.Error} passed from GetClientTokenAsync to OnCounterClicked in MainPage.xaml.cs";
                     }
 
                 }
                 catch (Exception ex) when (ex is OperationCanceledException)
                 {
-                    apiResponse.Text = $"{ex.Message} OperationCanceledException from GetClientTokenResponseAsync to OnCounterClicked in MainPage.xaml.cs";
+                    apiResponse.Text = $"{ex.Message} OperationCanceledException from GetClientTokenAsync to OnCounterClicked in MainPage.xaml.cs";
                 }
             }
         }
