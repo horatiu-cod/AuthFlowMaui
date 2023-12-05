21f344dfebf5dc95bdc655cf261abe7b0e07344b
horatiu <horatiu.cod@gmail.com>
2023-12-05 19:51:08 +0200

    add nullable

--- AuthFlowMaui.Api/Settings/JwtBearerConfig.cs
+++ AuthFlowMaui.Api/Settings/JwtBearerConfig.cs
@@ -2,13 +2,13 @@
 
 public class JwtBearerConfig
 {
-    public string Authority { get; set; }
+    public string? Authority { get; set; }
     public bool SaveToken { get; set; }
     public bool RequireHttpsMetadata { get; set; }
     public bool ValidateIssuer { get; set; }
     public bool ValidateAudience { get; set; }
-    public string ValidAudience { get; set; }
-    public string ValidIssuer { get; set; }
+    public string? ValidAudience { get; set; }
+    public string? ValidIssuer { get; set; }
     public bool ValidateLifetime { get; set; }
     public bool ValidateIssuerSigningKey { get; set; }
 }
