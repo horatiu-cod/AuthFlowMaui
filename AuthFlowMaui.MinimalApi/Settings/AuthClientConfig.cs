375a8a943453e34cd328b1d15ccdae0bfe755fca
horatiu <horatiu.cod@gmail.com>
2023-12-11 19:26:58 +0200

    add AuthClientConfig model

---
+++ AuthFlowMaui.MinimalApi/Settings/AuthClientConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthFlowMaui.MinimalApi.Settings;
+
+public class AuthClientConfig
+{
+    public string? ClientUuID { get; set; }
+    public string? ClientId { get; init; }
+    public string? ClientSecret { get; init; }
+    public string? Realm { get; init; }
+    public string? RealmUrl { get; set; }
+}
