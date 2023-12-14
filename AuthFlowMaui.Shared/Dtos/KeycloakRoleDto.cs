46d2a611c43fdb50cfb8efb8c39aa86f7fb1e63c
horatiu <horatiu.cod@gmail.com>
2023-12-14 09:47:03 +0200

    remove un-necessary prop

--- AuthFlowMaui.Shared/Dtos/KeycloakRoleDto.cs
+++ AuthFlowMaui.Shared/Dtos/KeycloakRoleDto.cs
@@ -5,23 +5,6 @@ namespace AuthFlowMaui.Shared.Dtos;
 public class KeycloakRoleDto
 {
     [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public string? Id { get; set; }
     [JsonPropertyName("name")]
     public string? Name { get; set; }
-    [JsonPropertyName("description")]
-    public string? Description { get; set; }
-    [JsonPropertyName("composite")]
-    public bool Composite { get; set; }
-    [JsonPropertyName("clientRole")]
-    public bool ClientRole { get; set; }
-    [JsonPropertyName("containerId")]
-    public string? ContainerId { get; set; }
-    [JsonPropertyName("attributes")]
-    public Attributes? Attributes { get; set; }
-}
-
-public class Attributes
-{
-    public string? Key { get; set; }
-    public string? Value { get; set; }
-}
