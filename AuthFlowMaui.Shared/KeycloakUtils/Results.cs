802d2d635c8368f11094aedf1bb8f994deea12a6
horatiu <horatiu.cod@gmail.com>
2023-12-14 09:42:16 +0200

    rename HttpStatusCode? to StatusCode

--- AuthFlowMaui.Shared/KeycloakUtils/Results.cs
+++ AuthFlowMaui.Shared/KeycloakUtils/Results.cs
@@ -2,21 +2,21 @@
 
 namespace AuthFlowMaui.Shared.KeycloakUtils
 {
-    public readonly record struct Result(bool IsSuccess, string? Error, HttpStatusCode? HttpStatus)
+    public readonly record struct Result(bool IsSuccess, string? Error, HttpStatusCode? StatusCode)
     {
         public static Result Success() => new(true, null, null);
-        public static Result Success(HttpStatusCode? httpStatus) => new(true, null, httpStatus);
+        public static Result Success(HttpStatusCode? StatusCode) => new(true, null, StatusCode);
         public static Result Fail(string? error) => new(false, error, null);
-        public static Result Fail(string? error, HttpStatusCode? httpStatus) => new(false, error, httpStatus);
+        public static Result Fail(string? error, HttpStatusCode? StatusCode) => new(false, error, StatusCode);
     }
 
-    public record struct Result<TData>(TData? Content, bool IsSuccess, HttpStatusCode? HttpStatus, string? Error)
+    public record struct Result<TData>(TData? Content, bool IsSuccess, HttpStatusCode? StatusCode, string? Error)
     {
         public static Result<TData> Success(TData? content) => new(content, true, null, null);
-        public static Result<TData> Success(TData? content, HttpStatusCode? HttpStatus) => new(content, true, HttpStatus, null);
-        public static Result<TData> Fail(HttpStatusCode HttpStatus) => new(default, false, HttpStatus, null);
+        public static Result<TData> Success(TData? content, HttpStatusCode? StatusCode) => new(content, true, StatusCode, null);
+        public static Result<TData> Fail(HttpStatusCode StatusCode) => new(default, false, StatusCode, null);
         //public static Result<TData> Fail( HttpStatusCode? HttpStatus, TData? content) => new(default, true, HttpStatus, null);
-        public static Result<TData> Fail(HttpStatusCode? HttpStatus, string? Error) => new(default, false, HttpStatus, Error);
+        public static Result<TData> Fail(HttpStatusCode? StatusCode, string? Error) => new(default, false, StatusCode, Error);
         public static Result<TData> Fail(string? Error) => new(default, false, null, Error);
     }
 }
