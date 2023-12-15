23ad5bfcec63d06d1bcb89ded93c66fe2501ffd7
horatiu <horatiu.cod@gmail.com>
2023-12-15 10:12:16 +0200

    change KeycloakRoleDto to array

--- AuthFlowMaui.Shared/Repositories/IApiRepository.cs
+++ AuthFlowMaui.Shared/Repositories/IApiRepository.cs
@@ -6,7 +6,7 @@ namespace AuthFlowMaui.Shared.Repositories
 {
     public interface IApiRepository
     {
-        Task<Result> AsignRoleToKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, KeycloakRoleDto keycloakRoleDto, HttpClient httpClient, string clientUuid, CancellationToken cancellationToken);
+        Task<Result> AsignRoleToKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, KeycloakRoleDto[] keycloakRoleDtos, HttpClient httpClient, string clientUuid, CancellationToken cancellationToken);
         Task<Result> DeletKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
         Task<Result<KeycloakRoleDto>> GetKeycloakRealmRole(KeycloakRoleDto keycloakRoleDto, KeycloakClientSettings clientSettings, HttpClient httpClient, string clientUuid, string roleName, CancellationToken cancellationToken);
         Task<Result<KeycloakUserDto>> GetKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
