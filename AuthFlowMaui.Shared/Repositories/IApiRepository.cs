0bfec697e58c51399bec764f9ba2a5b2dffa7f0a
horatiu <horatiu.cod@gmail.com>
2023-12-15 10:13:09 +0200

    remove unneccessary parameters

--- AuthFlowMaui.Shared/Repositories/IApiRepository.cs
+++ AuthFlowMaui.Shared/Repositories/IApiRepository.cs
@@ -8,8 +8,8 @@ namespace AuthFlowMaui.Shared.Repositories
     {
         Task<Result> AsignRoleToKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, KeycloakRoleDto[] keycloakRoleDtos, HttpClient httpClient, string clientUuid, CancellationToken cancellationToken);
         Task<Result> DeletKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
-        Task<Result<KeycloakRoleDto>> GetKeycloakRealmRole(KeycloakRoleDto keycloakRoleDto, KeycloakClientSettings clientSettings, HttpClient httpClient, string clientUuid, string roleName, CancellationToken cancellationToken);
-        Task<Result<KeycloakUserDto>> GetKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
+        Task<Result<KeycloakRoleDto>> GetKeycloakRealmRole(KeycloakClientSettings clientSettings, HttpClient httpClient, string clientUuid, string roleName, CancellationToken cancellationToken);
+        Task<Result<KeycloakUserDto>> GetKeycloakUser(string username, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
         Task<Result> RegisterKeycloakUser(KeycloakRegisterUserDto keycloakRegisterUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
         Task<Result> UpdateKeycloakUser(KeycloakUserDto keycloakUserDto, KeycloakClientSettings clientSettings, HttpClient httpClient, CancellationToken cancellationToken);
     }
