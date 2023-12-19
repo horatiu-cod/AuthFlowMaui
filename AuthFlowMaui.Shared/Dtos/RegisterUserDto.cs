e6e44601c55e739e55820621eb58a1f2f22e5c12
horatiu <horatiu.cod@gmail.com>
2023-12-19 12:49:27 +0200

    add RegisterUserDto

---
+++ AuthFlowMaui.Shared/Dtos/RegisterUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthFlowMaui.Shared.Dtos;
+
+public record struct RegisterUserDto()
+{
+    public string? Email { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public string? ClientId { get; set; }
+    public string? RoleName { get; set; }
+}
