d306dad0fa7d3f794d583381502ae87bd8655eb8
horatiu <horatiu.cod@gmail.com>
2023-12-19 12:42:51 +0200

    add KeycloakClientSettingsDto

---
+++ AuthFlowMaui.Shared/Dtos/KeycloakClientSettingsDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthFlowMaui.Shared.Dtos;
+
+internal record struct KeycloakClientSettingsDto()
+{
+    [JsonPropertyName("id")]
+    public string? ClientUuID { get; set; }
+    [JsonPropertyName("clientId")]
+    public string? ClientId { get; set; }
+}
