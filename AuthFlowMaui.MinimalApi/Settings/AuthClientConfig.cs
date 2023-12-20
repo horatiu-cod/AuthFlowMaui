559d20d1676aa9f768eed8463501d5bea9fa5eb5
horatiu <horatiu.cod@gmail.com>
2023-12-20 15:56:04 +0200

    remove props

--- AuthFlowMaui.MinimalApi/Settings/AuthClientConfig.cs
+++ AuthFlowMaui.MinimalApi/Settings/AuthClientConfig.cs
@@ -2,11 +2,8 @@
 
 public record struct AuthClientConfig()
 {
-    public string? ClientUuID { get; init; }
     public string? ClientId { get; init; }
     public string? ClientSecret { get; init; }
     public string? Realm { get; init; }
-    public string? RealmUrl { get; init; }
-
     public string? BaseUrl { get; init; }
 }
--- AuthFlowMaui.MinimalApi/Settings/ClientConfig.cs
+++ AuthFlowMaui.MinimalApi/Settings/ClientConfig.cs
@@ -4,7 +4,4 @@ public record struct ClientConfig
 {
     public string? ClientUuID { get; init; }
     public string? ClientId { get; init; }
-    public string? ClientSecret { get; init; }
-    public string? RealmUrl { get; init; }
-    public string? BaseUrl { get; init; }
 }
